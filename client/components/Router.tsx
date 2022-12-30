80701c57d3217f6e47e1b5eb10ef30213686d37c
pabloVoorvaart <pablov@graficadirecta.com>
2022-12-30 12:06:24 +0100

    router test

--- client/components/Router.tsx
+++ client/components/Router.tsx
@@ -5,44 +5,44 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Container from "./container";
 
 const Router = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-  const route = router.pathname;
-  const { isConnected, status } = useAccount();
-  const [isLoading, setIsLoading] = React.useState(true);
+  // const router = useRouter();
+  // const route = router.pathname;
+  // const { isConnected, status } = useAccount();
+  // const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    if (unprotectedRoutes.includes(route)) {
-      setIsLoading(false);
-      return;
-    }
-    setIsLoading(status === "reconnecting");
-  }, [isConnected, status]);
+  // React.useEffect(() => {
+  //   if (unprotectedRoutes.includes(route)) {
+  //     setIsLoading(false);
+  //     return;
+  //   }
+  //   setIsLoading(status === "reconnecting");
+  // }, [isConnected, status]);
 
-  const protectedRoutes = ["/", "/create", "/stream", "/golive"];
+  // const protectedRoutes = ["/", "/create", "/stream", "/golive"];
 
-  const unprotectedRoutes = ["/view"];
+  // const unprotectedRoutes = ["/view"];
 
-  if (isLoading) {
-    return (
-      <Container>
-        <h1 className="text-4xl font-bold">loading...</h1>
-      </Container>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <Container>
+  //       <h1 className="text-4xl font-bold">loading...</h1>
+  //     </Container>
+  //   );
+  // }
 
-  if (protectedRoutes.includes(route) && !isConnected) {
-    return (
-      <Container>
-        <h1 className="text-4xl font-bold">NFTS are live</h1>
-        <p className="text-xl text-center">
-          Welcome to NFTs are live! Connect your wallet to get started
-        </p>
-        <div className="w-40 m-auto">
-          <ConnectButton />
-        </div>
-      </Container>
-    );
-  }
+  // if (protectedRoutes.includes(route) && !isConnected) {
+  //   return (
+  //     <Container>
+  //       <h1 className="text-4xl font-bold">NFTS are live</h1>
+  //       <p className="text-xl text-center">
+  //         Welcome to NFTs are live! Connect your wallet to get started
+  //       </p>
+  //       <div className="w-40 m-auto">
+  //         <ConnectButton />
+  //       </div>
+  //     </Container>
+  //   );
+  // }
 
   return <>{children}</>;
 };
