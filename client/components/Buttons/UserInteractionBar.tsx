3fc730297a7265f85bcbc200911321a2955186e1
pabloVoorvaart <pablov@graficadirecta.com>
2022-12-30 12:35:17 +0100

    diasble subscribe button

--- client/components/Buttons/UserInteractionBar.tsx
+++ client/components/Buttons/UserInteractionBar.tsx
@@ -5,7 +5,6 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { ViewContext } from "context/viewContext";
 import Button from "./Button";
 const UserInteractionBar = () => {
-  const { isConnected } = useAccount();
   const context = React.useContext(ViewContext);
   if (!context) {
     return <div>loading</div>;
@@ -13,7 +12,7 @@ const UserInteractionBar = () => {
   const { address, displayVodContent, setDisplayVodContent } = context;
   return (
     <div className="flex flex-row space-x-2 m-3 justify-center">
-      {isConnected ? <MintButton address={address} /> : <ConnectButton />}
+      {/* {isConnected ? <MintButton address={address} /> : <ConnectButton />} */}
       {displayVodContent ? (
         <Button onClick={() => setDisplayVodContent(false)}>
           View Live Stream
