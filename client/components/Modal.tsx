7d946886a8be5f1ce47d9aa58527bd64a2c9f9f7
pabloVoorvaart <pablov@graficadirecta.com>
2023-01-02 16:53:00 +0100

    streamDetails component

--- client/components/Modal.tsx
+++ client/components/Modal.tsx
@@ -8,7 +8,7 @@ const Modal = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="flex absolute top-0 right-0 z-20 w-full h-full m-0">
+    <div className="flex absolute top-0 right-0 z-20 w-full h-full m-0 overflow-hidden">
       <div className=" bg-base-100 p-8 rounded-lg relative mt-4">
         <div
           className=" text-white absolute top-0 left-0 px-5 cursor-pointer"
--- client/pages/golive.tsx
+++ client/pages/golive.tsx
@@ -9,17 +9,17 @@ import UseObs from "components/goLive/UseObs";
 import UseTestStream from "components/goLive/UseTestSignal";
 import ViewComponent from "components/view/ViewComponent";
 import { ViewContextProvider } from "context/viewContext";
-
+import StreamDetails from "components/StreamDetails";
 const Create: NextPage = () => {
   const router = useRouter();
   const { address } = router.query;
-  const { stream } = useLiveNFT(address as string);
+  const { stream, lnftData } = useLiveNFT(address as string);
 
-  if (!stream || !address) return <div>Loading...</div>;
+  if (!stream || !address || !lnftData) return <div>Loading...</div>;
 
   return (
     <div className="flex h-screen lg:flex-row flex-col-reverse">
-      <div className="flex flex-col p-5 space-y-5 h-full lg:w-1/4 relative">
+      <div className="flex flex-col p-5 space-y-5 h-2/3 lg:h-full lg:w-1/4 relative overflow-auto">
         <Menu />
         <h1>Go live on your LNFT!</h1>
         <p>Stream directly into you LNFT using on of the following methods:</p>
@@ -27,6 +27,7 @@ const Create: NextPage = () => {
           <UseObs stream={stream} />
           <UseWebcam address={address as string} />
           <UseTestStream stream={stream} />
+          <StreamDetails details={lnftData} />
         </div>
       </div>
       <div className="flex flex-col h-1/3 lg:w-3/4 lg:h-full border-1">
