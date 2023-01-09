0a2e54e7f0ab8ddca5ce38fc4c0395642b3ebe09
pabloVoorvaart <pablov@graficadirecta.com>
2023-01-09 22:59:28 +0100

    name, router

--- client/components/create/Intro.tsx
+++ client/components/create/Intro.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import Button from "components/Buttons/Button";
 
-const Intro = ({onClick}: {onClick: () => void}) => {
+const Intro = ({ onClick }: { onClick: () => void }) => {
   return (
     <>
       <h1>Welcome to wave!</h1>
       <p>
-        Wave is an open, permisionless livestreaming platform
-        that allows content creators to easily monetize their content thanks to
-        the LiveNFT technology.
+        NFTsTv is an open, permisionless livestreaming platform that allows
+        content creators to easily monetize their content thanks to the LiveNFT
+        technology.
       </p>
       <p>Lets get started by creating your wave channel.</p>
-      <Button
-        onClick={onClick}
-      >
-        Create Channel
-      </Button>
+      <Button onClick={onClick}>Create Channel</Button>
     </>
   );
 };
