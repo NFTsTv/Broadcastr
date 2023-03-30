22d0ff6cc4f5c95c0e0b228e0ae386948fdb0531
Mario S.O <gh_mario.proclaim@slmail.me>
2023-03-30 14:14:04 -0400

    Better disable video SVG

--- client/components/Webcam/Controls.tsx
+++ client/components/Webcam/Controls.tsx
@@ -52,8 +52,7 @@ const Controls: React.FC<ControlsProps> = ({
               width="24"
               height="24"
             >
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M12.39 8L15 10.61V8zM21 6v12c0 1.1-.9 2-2 2H7c-1.1 0-2-.9-2-2V8c0-.55.45-1 1-1h4.5l1-1h7l1.73 1H20c.55 0 1 .45 1 1zm-2.5 9.5l-3.03-3.03L17 12.97zm-7.57-1.76l-1.55-1.55L9.9 10.5l-2.83-2.83L7.1 6.1l7.98 7.98-3.16 3.16z" />
+              <path d="M4 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6z" />
             </svg>
             Disable Video
           </Button>
