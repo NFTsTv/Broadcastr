f09f8fd8e4e7a3f26b877d769b86620a6b036481
pabloVoorvaart <me@pablovoorvaart.me>
2023-12-04 13:11:20 -0300

    create route

--- client/components/Elements/Routes/CreateRoute.tsx
+++ client/components/Elements/Routes/CreateRoute.tsx
@@ -9,7 +9,7 @@ const CreateRoute = ({ children }: { children: ReactNode }) => {
   const { castrAddress, loadingComplete } = useCastrAccount();
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    if (false) {
+    if (castrAddress) {
       router.push(Routes.CAST + "?address=" + castrAddress);
     } else if (loadingComplete) {
       setIsLoading(false);
